439e1716ab78a03608b9450a4d6f89f39b8bcadd
harsh9o9 <hbhardwaj454@gmail.com>
2023-11-10 16:16:01 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+let pagination = document.querySelector(".pagination");
+let list = $("ul");
+let firstBtn = $("ul li:first-child button");
+
+function setQueryParams() {
+  let links = document.querySelectorAll("a");
+
+  links.forEach((link) => {
+    let qString = $(link).attr("href") + `?page=${$(link).text()}`;
+    $(link).attr("href", qString);
+  });
+}
+function setPaginationFocus() {
+  let searchParams = new URLSearchParams(window.location.search);
+  if (searchParams.has("page")) {
+    let count = searchParams.get("page");
+
+    let toFocus = $(`.pagination a:eq(${count - 1})`);
+    toFocus.focus();
+  }
+  // $(firstBtn).focus();
+}
+
+setQueryParams();
+setPaginationFocus();
